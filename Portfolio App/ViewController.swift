d5f506a26cb2a376ad514d3137f17be9dcfe51c2
Sawyer Cherry <sawyercherry@macbook-pro-78.local>
2018-09-27 08:32:02 -0500

    Initial Commit

---
+++ Portfolio App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Portfolio App
+//
+//  Created by Sawyer Cherry on 9/27/18.
+//  Copyright © 2018 Sawyer Cherry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
